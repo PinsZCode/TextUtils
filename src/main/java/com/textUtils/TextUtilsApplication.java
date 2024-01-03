b0512a085830ccb28730f5c5da3c52dbdb78038e
Sanjay Pingua <89297690+PinsZCode@users.noreply.github.com>
2024-01-04 00:13:23 +0530

    127:create textUtils web application

---
+++ src/main/java/com/textUtils/TextUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.textUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/textUtils/TextUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textUtils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
